b57b3e6d6e9b05188241aa49e618cd94b462e16a
rikkejuliane <rikand01864@stud.noroff.no>
2025-10-08 13:14:00 +0200

    updated metadata

--- src/features/content/components/sections/ContactSection.tsx
+++ src/features/content/components/sections/ContactSection.tsx
@@ -2,7 +2,7 @@
 
 export default function ContactSection() {
   return (
-    <div className="grid h-full place-items-center text-center">
+    <div className="h-full w-full font-montserrat">
       <div>
         <h2 className="text-xl font-bold">Contact — Placeholder</h2>
         <p className="opacity-80">Drop a form or socials here.</p>
